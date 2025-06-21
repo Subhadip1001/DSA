2848f6cf079863a42e50ed2c84a584997258902f
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-06-21 20:29:58 +0530

    Min Stack

---
+++ Stack/minStack.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+// LeetCode: 155. Min Stack
+
+class MinStack {
+    stack<pair<int, int>> stk;
+public:
+    MinStack() {
+       
+    }
+    
+    void push(int val) {
+        if(stk.empty()){
+            stk.push({val, val});
+        } else{
+            int minVal = min(val, stk.top().second);
+            stk.push({val, minVal});
+        }
+    }
+    
+    void pop() {
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top().first;
+    }
+    
+    int getMin() {
+        return stk.top().second;
+    }
+};
+
+int main(){
+    MinStack minStack;
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    cout << "Minimum Element: " << minStack.getMin() << endl; // Returns -3
+    minStack.pop();
+    cout << "Top Element: " << minStack.top() << endl; // Returns 0
+    cout << "Minimum Element: " << minStack.getMin() << endl; // Returns -2
+
+    return 0;
+}
