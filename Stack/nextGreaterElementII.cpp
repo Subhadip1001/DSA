7281a838c1055d3f797fa6676b25c8bd90ee4a91
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-06-29 08:58:53 +0530

    Next Greater Element II

--- Stack/nextGreaterElementII.cpp
+++ Stack/nextGreaterElementII.cpp
@@ -1,48 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <unordered_map>
+#include<iostream>
+#include<vector>
+#include<stack>
 using namespace std;
 
-// Leetcode Problem: 496
-// Next Greater Element I
+// Leetcode Problem: 503
+// Next Greater Element II
 
-vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2){
-    unordered_map<int, int> map;
+vector<int> nextGreaterElement(vector<int>& nums){
+    int n = nums.size();
+    vector<int> ans(n, -1);
     stack<int> stk;
 
-    for (int i = nums2.size() - 1; i >= 0; i--){
-        while (!stk.empty() && stk.top() <= nums2[i]){
+    for(int i=2*n-1; i>=0; i--){
+        while(!stk.empty() && stk.top() <= nums[i%n]){
             stk.pop();
         }
 
-        if (stk.empty()){
-            map[nums2[i]] = -1;
-        }else{
-            map[nums2[i]] = stk.top();
-        }
-
-        stk.push(nums2[i]);
+        ans[i%n] = stk.empty() ? -1 : stk.top();
+        stk.push(nums[i%n]);
     }
-
-    int n = nums1.size();
-    vector<int> ans;
-
-    for (int i = 0; i < n; i++){
-        ans.push_back(map[nums1[i]]);
-    }
-
     return ans;
 }
 
 int main(){
-    vector<int> nums1 = {4, 1, 2};
-    vector<int> nums2 = {1, 3, 4, 2};
-
+    vector<int> nums = {1,2,3,4,3};
     vector<int> ans;
-    ans = nextGreaterElement(nums1, nums2);
+    ans = nextGreaterElement(nums);
     cout << "Next Greater Element: ";
-    for (int val : ans){
+    for(int val : ans){
         cout << val << " ";
     }
     return 0;
