0eb4f513fa97042ff258e22257275ab5b247da56
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-07-01 08:49:55 +0530

    Valid Palindrome

---
+++ Strings/vaildPalindrome.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+bool isAlphaNum(char ch){
+    if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+        return true;
+    }
+    return false;
+}
+
+bool isPalindrome(string str){
+    int s = 0, e = str.length()-1;
+
+    while (s<e){
+        if(!isAlphaNum(str[s])){ s++; continue;}
+        if(!isAlphaNum(str[e])){ e--; continue;}
+
+        if(tolower(str[s]) != tolower(str[e])){
+            return false;
+        }
+
+        s++; e--;
+    }
+    return true;
+}
+
+int main(){
+    string str = "Ac3?e3c&a";
+    // string str = "av{45uu";
+    bool ans = isPalindrome(str);
+    cout << ans << endl;
+    
+    return 0;
+}
