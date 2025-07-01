72d5f6f8711666600184dac1a029fe4d075a5eb8
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-07-01 09:44:28 +0530

    Remove All Occurrences of a Substring

--- Strings/removeAllOccurrences.cpp
+++ Strings/removeAllOccurrences.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+// LeetCode: 1910
+// Remove All Occurrences of a Substring
+
 string removeOccurrences(string s, string part){
     while(s.length() > 0 && s.find(part) <= s.length()){
         s.erase(s.find(part), part.length());
