bd469052b6675e887db4877a33a7e04cd14da744
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-07-03 10:11:24 +0530

    Permutation In String

--- Strings/permutationInString.cpp
+++ Strings/permutationInString.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+// LeetCode: 567
+// Permutation In String
+
 bool isFreqSame(int freq1[], int freq2[]){
     for(int i=0; i<26; i++){
         if(freq1[i] != freq2[i]){
